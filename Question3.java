32823d012bfbe26cdb3d710c6ae9d9938487fe79
WaJingJie <64137150+WaJingJie@users.noreply.github.com>
2020-04-25 17:23:29 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -10,7 +10,13 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner userinput = new Scanner(System.in);
     
+    System.out.println("Enter number: ");
+    double number = Double.parseDouble(userinput.nextLine());
+    
+    double multiply = number * number;
+    
+    System.out.println(multiply);
   }
 }
